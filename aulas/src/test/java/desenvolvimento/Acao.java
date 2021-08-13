2bdbc96f6a3f7012a2190a19a06d298516aacd51
ronal <ronal@DESKTOP-1ROGS41>
2021-08-13 16:11:05 -0300

    day 02 - 13/08/2021

--- aulas/src/test/java/desenvolvimento/Acao.java
+++ aulas/src/test/java/desenvolvimento/Acao.java
@@ -1,34 +1,44 @@
 package desenvolvimento;
 
+import java.util.Random;
 
 public class Acao {
 	private boolean acesso = false;
 	private Integer fichas = 0;
+	private Integer n1;
+	private Integer n2;
+	private Integer n3;
+
 	
 	
 	
 	public void puxarAlavanca(Integer fichas) {
-		if (this.fichas !=0) {
-			
+		if (fichas >0) {
+			Random r1 = new Random();
+			Random r2 = new Random();
+			Random r3 = new Random();
+			this.n1 = r1.nextInt(3);
+			this.n2 = r2.nextInt(3);
+			this.n3 = r3.nextInt(3);
+
+			System.out.println(n1 + " " + n2 + " " + n3);
 		}
 	}
-	
+			
+			
 	
 	
 	public void colocarMoeda(Jogador moedaDoJogador) {
 		switch (moedaDoJogador.getMoeda()) {
 			case 1:
-				System.out.print("Você tem 1 chance");
 				this.fichas = 1;
 				this.acesso = true;
 				break;
 			case 5:
-				System.out.print("Você tem 5 chances");
 				this.fichas = 5;
 				this.acesso = true;
 				break;
 			case 10:
-				System.out.print("Você tem 10 chances");
 				this.fichas = 10;
 				this.acesso = true;
 				break;
@@ -46,4 +56,31 @@ public class Acao {
 	public Integer getFichas() {
 		return fichas;
 	}
+
+
+	public Integer getN1() {
+		return n1;
+	}
+	public void setN1(Integer n1) {
+		this.n1 = n1;
+	}
+
+	public Integer getN2() {
+		return n2;
+	}
+	public void setN2(Integer n2) {
+		this.n2 = n2;
+	}
+
+	public Integer getN3() {
+		return n3;
+	}
+	public void setN3(Integer n3) {
+		this.n3 = n3;
+	}
+
+
+
+
+
 }
--- aulas/src/test/java/desenvolvimento/Maquina.java
+++ aulas/src/test/java/desenvolvimento/Maquina.java
@@ -2,36 +2,49 @@ package desenvolvimento;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.util.Random;
 
-public class Maquina extends Acao{
+public class Maquina{
 	private Integer fichas = 0;
 	private boolean acesso = false;
+	private Integer n1;
+	private Integer n2;
+	private Integer n3;
 	
 	@Test
 	public void testeGerarAcesso() throws Exception {
-		Acao acao = new Acao();
+		Acao maquina = new Acao();
 		Jogador jogador = new Jogador();
 		jogador.setMoeda(5);
-		acao.colocarMoeda(jogador);
-		acesso = acao.getAcesso();
-		fichas = acao.getFichas();
+		maquina.colocarMoeda(jogador);
+		acesso = maquina.getAcesso();
+		fichas = maquina.getFichas();
 		
 		assertEquals(Integer.valueOf(5), jogador.getMoeda());
 		assertEquals(Integer.valueOf(5), fichas);
 		assertTrue(acesso);
 		
+		while (this.fichas >0) {
+			System.out.println("Você possui: " + fichas + " fichas");
+			maquina.puxarAlavanca(fichas);
+			n1 = maquina.getN1();
+			n2 = maquina.getN2();
+			n3 = maquina.getN3();
+			
+			this.fichas = fichas - 1;
+			
+		}
+			
+		
+		
 	}
 	
 	public void testePuxarAlavanca() throws Exception {
 		
-		if (acesso = true) {
-			
-			
+		
 				
 		}
-	}
 
-	
 	
 	
 	
