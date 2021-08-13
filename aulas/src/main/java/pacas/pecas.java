8d9ced31454533bb5c371d42729d4e022a0e7904
T-Gamer <T-Gamer@DESKTOP-U26BJ62>
2021-08-13 20:12:31 -0300

    Peças do jogo feitas colocando um sistema de Hierarquia em uma superclasse abstrata

---
+++ aulas/src/main/java/pacas/pecas.java
@@ -0,0 +1,9 @@
+package pacas;
+
+public abstract class pecas {
+
+	public pecas() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ aulas/src/main/java/pacas/torres.java
@@ -0,0 +1,5 @@
+package pacas;
+
+public class torres {
+
+}
