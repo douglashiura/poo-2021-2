7d91567d5bd7a84c750d86500bfa9a92d4eeeb30
Thiago <Thiago@192.168.0.5>
2021-08-18 20:25:38 -0300

    Aula 1 JOptionPane

---
+++ aulas/JOptionPane.java
@@ -0,0 +1,10 @@
+
+import javax.swing.JOptionPane;
+
+public class Aula1 {
+	public class TestaDialogo {
+		public static void main(String args[]) {
+		 JOptionPane.showMessageDialog(null,"Este é um diálogo simples");
+		}
+	}
+}
