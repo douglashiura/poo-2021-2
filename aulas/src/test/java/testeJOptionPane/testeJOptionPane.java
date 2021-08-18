a617a4f1320a3b9c6f608a9dc1e1912064ef966a
deiwidSITE <deiwidpetrydvd@gmail.com>
2021-08-18 20:21:06 -0300

    Primeiro commit JOptionPane.

---
+++ aulas/src/test/java/testeJOptionPane/testeJOptionPane.java
@@ -0,0 +1,12 @@
+package testeJOptionPane;
+import javax.swing.JOptionPane;
+
+import org.junit.Test;
+
+public class testeJOptionPane {
+
+	@Test
+	public void testeInput() throws Exception {
+		JOptionPane.showInputDialog("Teste Deiwid");
+	}
+}
