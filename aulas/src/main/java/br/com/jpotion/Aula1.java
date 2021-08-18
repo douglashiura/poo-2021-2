759fd2c61b8e50a29d02f3f1621f5033b5c6fd59
nnilocoelho <nnilocoelho@gmail.com>
2021-08-18 20:39:13 -0300

    commit 18.08: "aula1 joptionpane"

---
+++ aulas/src/main/java/br/com/jpotion/Aula1.java
@@ -0,0 +1,9 @@
+package br.com.jpotion;
+import javax.swing.JOptionPane;
+
+public class Aula1 {
+	public static void main (String args[]) {
+		JOptionPane.showMessageDialog(null, "Hello, World! Olá Mundo!, Shalam Aleiko");
+	}
+
+}
