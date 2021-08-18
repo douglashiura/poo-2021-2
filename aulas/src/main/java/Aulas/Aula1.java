db2cc9d44105821ae81935cbe20c4257f01dbff1
Matheus-Alborghetti <Matheus-Alborghetti@users.noreply.github.com>
2021-08-18 20:28:01 -0300

    JoptionPane teste

---
+++ aulas/src/main/java/Aulas/Aula1.java
@@ -0,0 +1,11 @@
+package Aulas;
+import javax.swing.JOptionPane;
+
+public class Aula1 {
+
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog("Teste Caixa de Diálogo");
+				
+	}
+
+}
