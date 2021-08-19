38674874c750e41668d042ebf58d0b79dd9f04dc
LuisHenriqueAmaroBorges <80133873+LuisHenriqueAmaroBorges@users.noreply.github.com>
2021-08-18 22:01:04 -0300

    Primeira Aula de JFrame

--- aulas/src/main/java/aula/FramePanel.java
+++ aulas/src/main/java/aula/FramePanel.java
@@ -1,5 +1,13 @@
 package aula;
 
+
+import javax.swing.JFrame;
+
 public class FramePanel {
+	public static void main(String[] args) {
+		JFrame janela = new JFrame("Meu primeiro frame em Java");
+		janela.setSize(300, 200);
+		janela.setVisible(true);
+	}
 
 }
