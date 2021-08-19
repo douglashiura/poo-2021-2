947194f442c72716f7e66e2063d2a2a386dbc646
LuisHenriqueAmaroBorges <80133873+LuisHenriqueAmaroBorges@users.noreply.github.com>
2021-08-18 21:01:17 -0300

    Primeiro JOptionPane

--- aulas/src/main/java/aula/TesteEntradaSaida.java
+++ aulas/src/main/java/aula/TesteEntradaSaida.java
@@ -5,8 +5,8 @@ import javax.swing.JOptionPane;
 
 public class TesteEntradaSaida{
 	
-	public static void main(String{} args){
-	String numero=JOptionPane.showMessageDialog("digite um numero:");
+	public static void main(String{} args) {
+	String numero=JOptionPane.showInputDialog("digite um numero:");
 			JOptionPane.showMessageDialog(null,"o numero digitado foi:"+ numero);
 
 	}
