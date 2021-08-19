87799f738f0818830a7ceacafc240a45d2b0a7f6
Matheus-Alborghetti <Matheus-Alborghetti@users.noreply.github.com>
2021-08-18 21:44:00 -0300

    Hello World Swing

---
+++ aulas/src/main/java/Aulas/HelloWorldSwing.java
@@ -0,0 +1,13 @@
+package Aulas;
+import javax.swing.*;
+
+public class HelloWorldSwing {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("HelloWorldSwing");
+		final JLabel label = new JLabel("Hello World");
+		frame.getContentPane() .add(label);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+		}
+	}
