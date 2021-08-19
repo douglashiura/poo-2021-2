701757bde13bfb843c6777232dac84f47c86cdcd
nnilocoelho <nnilocoelho@gmail.com>
2021-08-18 21:52:34 -0300

    commit aula1 JavaxSwing

---
+++ aulas/src/main/java/br/com/jpotion/Aula1JSwing.java
@@ -0,0 +1,17 @@
+package br.com.jpotion;
+
+import javax.swing.*;
+
+public class Aula1JSwing {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Hello World Swing");
+		final JLabel label = new JLabel("Hello World");
+		frame.getContentPane().add(label);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+
+	}
+
+}
