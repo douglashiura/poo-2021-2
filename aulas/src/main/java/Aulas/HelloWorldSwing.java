8915dd00b6c7ed88dc267a09aee69d2caf7c3386
Matheus-Alborghetti <Matheus-Alborghetti@users.noreply.github.com>
2021-08-18 21:45:37 -0300

    Alteração do tamanho

--- aulas/src/main/java/Aulas/HelloWorldSwing.java
+++ aulas/src/main/java/Aulas/HelloWorldSwing.java
@@ -3,10 +3,11 @@ import javax.swing.*;
 
 public class HelloWorldSwing {
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("HelloWorldSwing");
+		JFrame frame = new JFrame("JNela é isso!");
 		final JLabel label = new JLabel("Hello World");
 		frame.getContentPane() .add(label);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 300);
 		frame.pack();
 		frame.setVisible(true);
 		}
