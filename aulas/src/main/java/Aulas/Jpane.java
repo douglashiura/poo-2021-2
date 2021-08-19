44bae0012623eb4cc6889bf15d7de3e1b59de6a7
Matheus-Alborghetti <Matheus-Alborghetti@users.noreply.github.com>
2021-08-18 21:31:06 -0300

    Jrame Ok

---
+++ aulas/src/main/java/Aulas/Jpane.java
@@ -0,0 +1,10 @@
+package Aulas;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class Jpane {
+	public static void main(String[] args) {
+		 JFrame janela = new JFrame("Jframe na Jnela");
+		 janela.setVisible(true);
+		    }
+		}
