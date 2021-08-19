d5d5c7840c850f88caf8e5350be3b9d7bad1ae9b
Larissa <Larissa@DESKTOP-IPLM3KF>
2021-08-18 22:19:37 -0300

    teste Frame

--- aulas/src/main/java/testeJOptionPane/Teste.java
+++ aulas/src/main/java/testeJOptionPane/Teste.java
@@ -1,9 +1,20 @@
 package testeJOptionPane;
 
-import javax.swing.JOptionPane;
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Teste {
 	public static void main (String arg[]) {
-		JOptionPane.showInputDialog("Teste");
+		//JOptionPane.showInputDialog("Teste");
+		
+		JFrame frame = new JFrame("Frame");
+		JLabel label = new JLabel("Teste");
+		//frame.setSize(400, 300);
+		frame.getContentPane().add(label, BorderLayout.CENTER);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 }
