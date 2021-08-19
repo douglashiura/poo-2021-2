3ec32316f0798bbc6b72282f51bd93eb8a3c9f19
atn337 <38930758+atn337@users.noreply.github.com>
2021-08-18 21:32:29 -0300

    Primeiro Jframe.

--- aulas/src/test/java/aulas/TestJOptionPane.java
+++ aulas/src/test/java/aulas/TestJOptionPane.java
@@ -1,13 +1,18 @@
 package aulas;
 
-import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+//import javax.swing.JOptionPane;
 
 public class TestJOptionPane {
 
 	public static void main(String arg[]) {
 
-		JOptionPane.showInputDialog(null, "Olaaaa");
+		// JOptionPane.showInputDialog(null, "Olaaaa");
 
+		JFrame janela = new JFrame("Testando");
+		janela.setSize(300, 200);
+		janela.setVisible(true);
+		
+		
 	}
-
 }
