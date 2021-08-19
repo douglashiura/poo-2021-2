bb0801ad5e7cc5dd8d55256c62fbb3fc840c8e85
nnilocoelho <nnilocoelho@gmail.com>
2021-08-19 10:43:37 -0300

    commit 19.04: pacotes MVC

---
+++ aulas/src/main/java/br/com/jpotion/Aula1ContentPane.java
@@ -0,0 +1,14 @@
+package br.com.jpotion;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Aula1ContentPane {
+	JFrame frame = new JFrame("Teste");
+	JLabel label = new JLabel("Teste II");
+	
+	
+		
+}
