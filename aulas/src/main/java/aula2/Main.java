6604bef06e6d019172cdb7f3e9b653940846237c
carlos <carlos@Jedi>
2021-08-20 20:13:50 -0300

    Trabalho 1

--- aulas/src/main/java/aula2/Main.java
+++ aulas/src/main/java/aula2/Main.java
@@ -1,18 +1,24 @@
 package aula2;
 
+import java.awt.Label;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class Main {
-	Object[] options = {"Sim!", "Não", "Quem sabe?"};
-	JFrame frame = new JFrame("Hello Word");
-	int n = JOptionPane.showOptionDialog(
-			frame, "Tem certeza?",
-			"Confirmação",
-			JOptionPane.YES_NO_CANCEL_OPTION,
-			JOptionPane.QUESTION_MESSAGE,
-			null,
-			options,
-			options[2]
-	);
+	public static void main(String[] args) {
+		Object[] options = {"Sim!", "Não", "Quem sabe?"};
+		JFrame frame = new JFrame("Hello Word");
+		int n = JOptionPane.showOptionDialog(
+				frame, "Tem certeza?",
+				"Confirmação",
+				JOptionPane.YES_NO_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE,
+				null,
+				options,
+				options[2]
+		);
+		frame.getContentPane().add(new Label(options[n].toString()));
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
