e7144a221320aab8545f39219e10a46a46abb0fb
ronal <ronal@DESKTOP-1ROGS41>
2021-08-20 20:28:33 -0300

    atividade 01

--- aulas/src/test/java/testesemaula/InterfaceGrafica.java
+++ aulas/src/test/java/testesemaula/InterfaceGrafica.java
@@ -1,6 +1,5 @@
 package testesemaula;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 
 import javax.swing.JFrame;
@@ -15,17 +14,21 @@ public class InterfaceGrafica {
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(500, 500); // pixels da tela
-		
+
 		JLabel label = new JLabel("Colocar conteúdo de texto, também pode ser uma IMG pois existe construtor pra isso");
 		label.setVisible(false);
 		frame.getContentPane().add(label); // ??
-		
+
 		label.setBackground(Color.black);
-		label.setForeground(Color.GREEN.darker()); //cor cinza escuro
+		label.setForeground(Color.GREEN.darker()); // cor cinza escuro
 		label.setOpaque(true); // mostrar ou não a cor do label
 		
 		
-		//JOptionPane.showMessageDialog(null, label);
+		
+		
+		
 	}
-
-}
+	
+	
+}	
+	
