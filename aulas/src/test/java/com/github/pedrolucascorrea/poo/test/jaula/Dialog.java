62b9fdfb2a6c5e229b6fd4445eccca93fba63bed
pedro <pedro@pedro-desktop>
2021-08-20 20:27:57 -0300

    Usando JButton

--- aulas/src/test/java/com/github/pedrolucascorrea/poo/test/jaula/Dialog.java
+++ aulas/src/test/java/com/github/pedrolucascorrea/poo/test/jaula/Dialog.java
@@ -1,7 +1,9 @@
 package com.github.pedrolucascorrea.poo.test.jaula;
 
-import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
@@ -9,13 +11,23 @@ public class Dialog {
 	public static void main(String[] args) {
 		Object[] options = { "Sim!", "Não", "Quem sabe?" };
 		JFrame frame = new JFrame("Hello");
+		JButton button = new JButton("Clique aqui para abrir o modal");
+		button.addActionListener(new ActionListener() {
 
-		int n = JOptionPane.showOptionDialog(frame, "Tem certeza?", "Confirmação", JOptionPane.YES_NO_CANCEL_OPTION,
-				JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				int n = JOptionPane.showOptionDialog(frame, "Tem certeza?", "Confirmação",
+						JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+				frame.setTitle(options[n].toString());
 
-		frame.getContentPane().add(new Label(options[n].toString()));
-		frame.pack();
-		frame.setVisible(Boolean.TRUE);
+			}
+
+		});
+
+		frame.getContentPane().add(button);
+		frame.setSize(600, 600);
+		frame.setVisible(true);
+
 	};
 
 };
