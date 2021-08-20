c7ac0a023547b922551fcb6d4de47d8d11cbe952
carlos <carlos@Jedi>
2021-08-20 20:28:55 -0300

    Exercicio 1

--- aulas/src/main/java/aula2/Main.java
+++ aulas/src/main/java/aula2/Main.java
@@ -1,6 +1,9 @@
 package aula2;
 
-import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
@@ -8,17 +11,25 @@ public class Main {
 	public static void main(String[] args) {
 		Object[] options = {"Sim!", "Não", "Quem sabe?"};
 		JFrame frame = new JFrame("Hello Word");
-		int n = JOptionPane.showOptionDialog(
-				frame, "Tem certeza?",
-				"Confirmação",
-				JOptionPane.YES_NO_CANCEL_OPTION,
-				JOptionPane.QUESTION_MESSAGE,
-				null,
-				options,
-				options[2]
-		);
-		frame.getContentPane().add(new Label(options[n].toString()));
-		frame.pack();
+		JButton button = new JButton("Clique aqui, para abrir o modal");
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int n = JOptionPane.showOptionDialog(
+						frame, "Tem certeza?",
+						"Confirmação",
+						JOptionPane.YES_NO_CANCEL_OPTION,
+						JOptionPane.QUESTION_MESSAGE,
+						null,
+						options,
+						options[2]
+				);
+				frame.setTitle(options[n].toString());
+				
+			}
+		});
+		frame.getContentPane().add(button);
+		frame.setSize(600,600);
 		frame.setVisible(true);
 	}
 }
