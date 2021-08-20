a721715fdcd02c7e39f00499e024af2f378d55bf
carlos <carlos@Jedi>
2021-08-20 20:02:01 -0300

    Aula2 V2

--- aulas/src/main/java/aula2/Main.java
+++ aulas/src/main/java/aula2/Main.java
@@ -1,11 +1,11 @@
 package aula2;
 
-import java.awt.Component;
-
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class Main {
 	Object[] options = {"Sim!", "Não", "Quem sabe?"};
+	JFrame frame = new JFrame("Hello Word");
 	int n = JOptionPane.showOptionDialog(
 			frame, "Tem certeza?",
 			"Confirmação",
