e35f97f77b239f41512dde8cc8cd5ecba718ce52
Lucas-l07 <luucasbraatz@gmail.com>
2021-08-20 20:35:01 -0300

    Att JOptionPane Dialog

--- aulas/src/test/java/teste/aula02/TestJOptionPaneDialog.java
+++ aulas/src/test/java/teste/aula02/TestJOptionPaneDialog.java
@@ -1,25 +1,30 @@
 package teste.aula02;
-import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 public class TestJOptionPaneDialog {
 	public static void main(String[] args) {
+		Object[] options = {"Sim", "Não", "Quem sabe?"};
 		JFrame frame = new JFrame("TESTE DE FRAME");
-		final JLabel label = new JLabel("JANELA ABERTA");
-		frame.getContentPane().add(label, BorderLayout.CENTER);
+		JButton button = new JButton("Clique aqui!");
+		button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int n = JOptionPane.showOptionDialog(frame, "Tem certeza?", "Confirmação",
+						JOptionPane.YES_NO_CANCEL_OPTION,
+						JOptionPane.QUESTION_MESSAGE,
+						null,
+						options,
+						options[2]);
+				frame.setTitle(options[n].toString());
+			}
+		});
+		frame.getContentPane().add(button);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
+		frame.setSize(300, 200);
 		frame.setVisible(true);
-		
-		Object[] options = {"Sim!", "Não", "Quem sabe?"};
-		int n = JOptionPane.showOptionDialog(frame, "Tem certeza?", "Confirmação",
-				JOptionPane.YES_NO_CANCEL_OPTION,
-				JOptionPane.QUESTION_MESSAGE,
-				null,
-				options,
-				options[2]);
-		
-		System.out.println(n);
 	}
 }
