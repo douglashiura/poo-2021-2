03c2d9978d9c82a71e38cc37d3734967a2aac148
Matheus-Alborghetti <Matheus-Alborghetti@users.noreply.github.com>
2021-08-20 19:56:37 -0300

    aula swing 2 exemplo de hierarquia de janela

---
+++ aulas/src/main/java/Aulas/TestJOptionPaneDialog.java
@@ -0,0 +1,25 @@
+package Aulas;
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+
+public class TestJOptionPaneDialog {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("TESTE DE FRAME");
+		final JLabel label = new JLabel("JANELA ABERTA");
+		frame.getContentPane().add(label, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+
+		Object[] options = {"Sim!", "Não", "Quem sabe?"};
+		int n = JOptionPane.showOptionDialog(frame, "Tem certeza?", "Confirmação",
+				JOptionPane.YES_NO_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE,
+				null,
+				options,
+				options[2]);
+
+		System.out.println(n);
+	}
+}
