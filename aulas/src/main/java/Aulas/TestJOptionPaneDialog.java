55830bbfed0b159c981abc0bfb91a25f20388f70
Matheus-Alborghetti <Matheus-Alborghetti@users.noreply.github.com>
2021-08-20 20:15:52 -0300

    Atividade 1

--- aulas/src/main/java/Aulas/TestJOptionPaneDialog.java
+++ aulas/src/main/java/Aulas/TestJOptionPaneDialog.java
@@ -1,25 +1,23 @@
 package Aulas;
-import java.awt.BorderLayout;
-
-import javax.swing.*;
+import java.awt.Label;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class TestJOptionPaneDialog {
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("JFrame de Jnela");
-		final JLabel label = new JLabel("Jnela é isso!");
-		frame.getContentPane().add(label, BorderLayout.CENTER);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
-		frame.setVisible(true);
-
 		Object[] options = {"Sim!", "Não", "Quem sabe?"};
-		int n = JOptionPane.showOptionDialog(frame, "Tem certeza?", "Confirmação",
+		JFrame frame = new JFrame("Hello Word");
+		int n = JOptionPane.showOptionDialog(
+				frame, "Tem certeza?",
+				"Confirmação",
 				JOptionPane.YES_NO_CANCEL_OPTION,
 				JOptionPane.QUESTION_MESSAGE,
 				null,
 				options,
-				options[2]);
-
-		System.out.println(n);
+				options[2]
+		);
+		frame.getContentPane().add(new Label(options[n].toString()));
+		frame.pack();
+		frame.setVisible(true);
 	}
 }
