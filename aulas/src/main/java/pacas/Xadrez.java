403a55665f43866f59a14b1b277e8edaaffd570a
T-Gamer <T-Gamer@DESKTOP-U26BJ62>
2021-08-20 19:31:14 -0300

    Comit que faltou no dia 18

---
+++ aulas/src/main/java/pacas/Xadrez.java
@@ -0,0 +1,15 @@
+package pacas;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Xadrez {
+    public static void main(String[] args) {        
+        Scanner userInputScanner = new Scanner(System.in);
+        Jogo charada = new Jogo("Boa sorte");
+        String entrada;       
+        System.out.println("Bem vindo");
+        
+    } 
+
+
+
