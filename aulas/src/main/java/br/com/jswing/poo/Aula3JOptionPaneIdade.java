29518672de87d982bc5686e1d4d1c518c8b6c4d5
nnilocoelho <nnilocoelho@gmail.com>
2021-08-25 19:58:18 -0300

    Commit 25.08: Aula 3 JOptionPaneIdade

--- aulas/src/main/java/br/com/jswing/poo/Aula3JOptionPaneIdade.java
+++ aulas/src/main/java/br/com/jswing/poo/Aula3JOptionPaneIdade.java
@@ -10,7 +10,7 @@ public class Aula3JOptionPaneIdade {
 	public static void main(String[] args) {
 		Object[] options = { "10", "20", "30", "40" };
 		JFrame frame = new JFrame("Idade");
-		int n = JOptionPane.showOptionDialog(frame, "Qual sua idade?", "Selecione sua idade",
+		int n = JOptionPane.showOptionDialog(frame, "Qual é sua idade?", "Selecione sua idade",
 				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
 		frame.getContentPane().add(new Label(options[n].toString()));
 		frame.setSize(400, 300);
