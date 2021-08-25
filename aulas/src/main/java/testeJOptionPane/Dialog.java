1fa507001dbfdabffe12c5d825bf2ac1ba7bc5eb
Larissa <Larissa@DESKTOP-IPLM3KF>
2021-08-25 19:40:06 -0300

    aula 03 - modal idade

--- aulas/src/main/java/testeJOptionPane/Dialog.java
+++ aulas/src/main/java/testeJOptionPane/Dialog.java
@@ -7,12 +7,12 @@ import javax.swing.JOptionPane;
 
 public class Dialog {
 	public static void main (String arg[]) {
-		Object[] options = { "Sim!", "Não", "Quem sabe?" };
+		Object[] options = { "10", "20", "30", "40" };
 		JFrame frame = new JFrame("Frame");
 		frame.setVisible(Boolean.TRUE);
-		int n = JOptionPane.showOptionDialog(frame, "Tem certeza?", "Confirmação", JOptionPane.YES_NO_CANCEL_OPTION,
+		int n = JOptionPane.showOptionDialog(frame, "Qual a sua idade?", "Idade", JOptionPane.YES_NO_CANCEL_OPTION,
 		JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
 		frame.getContentPane().add(new Label(options[n].toString()));
-		frame.setSize(400, 200);
+		frame.setSize(300, 100);
 	}
 }
