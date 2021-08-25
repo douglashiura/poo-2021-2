b4a6fc448cdb5e3ca1bcebf6b890788c3c1056e8
nnilocoelho <nnilocoelho@gmail.com>
2021-08-25 19:53:39 -0300

    Commit 25.08: Aula 3 JOptionPaneIdade

--- aulas/src/main/java/br/com/jswing/poo/Aula2JOptionPane.java
+++ aulas/src/main/java/br/com/jswing/poo/Aula2JOptionPane.java
@@ -1,17 +1,28 @@
 package br.com.jswing.poo;
 
-import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class Aula2JOptionPane {
-
 	public static void main(String[] args) {
-		Object[] options = { "Sim", "Não", "É memo é?" };
-		Component frame = null;
-		int n = JOptionPane.showOptionDialog(frame, "Tem certeza?", "Confirmação", JOptionPane.YES_NO_CANCEL_OPTION,
-				JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+		Object[] options = { "Sim!", "Não", "Quem sabe?" };
+		JFrame frame = new JFrame("Hello Word");
+		JButton button = new JButton("Abrir Modal.");
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int n = JOptionPane.showOptionDialog(frame, "Tem certeza?", "Confirmação",
+						JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+				frame.setTitle(options[n].toString());
 
+			}
+		});
+		frame.getContentPane().add(button);
+		frame.setSize(600, 600);
+		frame.setVisible(true);
 	}
-
 }
