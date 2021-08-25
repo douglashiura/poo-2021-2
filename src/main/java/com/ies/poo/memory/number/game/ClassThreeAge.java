a0721a6ad083a92dd2a1f6b4bdc24ebbc205d841
Proctux <luiz.ado@hotmail.com>
2021-08-25 19:43:12 -0300

    feat: Add class two and class three files

---
+++ src/main/java/com/ies/poo/memory/number/game/ClassThreeAge.java
@@ -0,0 +1,24 @@
+package com.ies.poo.memory.number.game;
+
+import java.awt.Label;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class ClassThreeAge {
+
+	public static void main(String[] args) {
+		Object[] options = { "< 10", "< 20", "< 30", "< 40", "> 41" };
+		JFrame frame = new JFrame("Age frame");
+		int optionIndex = JOptionPane.showOptionDialog(frame, 
+				"Select your age from the options below", 
+				"Confirm", 
+				JOptionPane.YES_NO_CANCEL_OPTION, 
+				JOptionPane.QUESTION_MESSAGE, null, options, null);
+		frame.getContentPane().add(new Label(options[optionIndex].toString()));
+		frame.setSize(400, 300);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
---
+++ src/main/java/com/ies/poo/memory/number/game/ClassTwoJOptionPane.java
@@ -0,0 +1,21 @@
+package com.ies.poo.memory.number.game;
+
+import java.awt.Label;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class ClassTwoJOptionPane {
+
+	public static void main(String[] args) {
+		Object[] options = { "Sim", "Não", "Talvez" };
+		JFrame frame = new JFrame("Frame title");
+		int n = JOptionPane.showOptionDialog(frame, "Selecione uma das opções", "Confirmação", JOptionPane.YES_NO_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE, null, options, null);
+		
+		frame.getContentPane().add(new Label(options[n].toString()));
+		frame.setSize(400, 300);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+	}
+}
