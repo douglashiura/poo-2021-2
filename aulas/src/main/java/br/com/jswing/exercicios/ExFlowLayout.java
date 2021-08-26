56343118f87f372af9e42cf1576e08bc81cb5e83
nnilocoelho <nnilocoelho@gmail.com>
2021-08-26 01:25:16 -0300

    Commit 26.08 - Entrega Exercicios Layout Manager.

--- aulas/src/main/java/br/com/jswing/exercicios/ExFlowLayout.java
+++ aulas/src/main/java/br/com/jswing/exercicios/ExFlowLayout.java
@@ -1,5 +1,25 @@
 package br.com.jswing.exercicios;
 
+import java.awt.*;
+import javax.swing.*;
+
 public class ExFlowLayout {
+
+	public static void main(String args[]) {
+
+		JFrame frame = new JFrame();
+		frame.setTitle("Flow Layout");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 100);
+		frame.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+
+		frame.add(new JButton("Botão 1"));
+		frame.add(new JButton("Botão 2"));
+		frame.add(new JButton("Botão 3"));
+		frame.add(new JButton("Botão 4"));
+
+		frame.setVisible(true);
+
+	}
 
 }
