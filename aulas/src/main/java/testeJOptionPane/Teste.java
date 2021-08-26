4f5f8823853d02af2660bff60562f6cfdbdd7e31
Larissa <Larissa@DESKTOP-IPLM3KF>
2021-08-26 20:17:11 -0300

    atividade BoxLayout

--- aulas/src/main/java/testeJOptionPane/Teste.java
+++ aulas/src/main/java/testeJOptionPane/Teste.java
@@ -1,22 +1,36 @@
 package testeJOptionPane;
 
-import java.awt.BorderLayout;
-
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 
 public class Teste {
 	public static void main (String arg[]) {
-		//JOptionPane.showInputDialog("Teste");
-		
 		JFrame frame = new JFrame("Frame");
-		JLabel label = new JLabel("Teste");
-		//frame.setSize(400, 300);
-		label.setVisible(false);
-		frame.getContentPane().add(label, BorderLayout.CENTER);
-		frame.pack();
+		
+		BoxLayout boxLayout = new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS);
+		frame.setLayout(boxLayout);
+		
+		JLabel label1 = new JLabel("Label 1", JLabel.CENTER);
+		JLabel label2 = new JLabel("Label 2", JLabel.CENTER);
+		JLabel label3 = new JLabel("Label 3", JLabel.CENTER);
+		JLabel label4 = new JLabel("Label 4", JLabel.CENTER);
+		JLabel label5 = new JLabel("Label 5", JLabel.CENTER);
+		
+		label1.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		label2.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		label3.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		label4.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		label5.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		
+		frame.add(label1);
+		frame.add(label2);
+		frame.add(label3);
+		frame.add(label4);
+		frame.add(label5);
+				
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		frame.pack();
 	}
 }
