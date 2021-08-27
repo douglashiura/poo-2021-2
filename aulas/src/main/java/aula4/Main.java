e758c41b975679af7c659fce8b9eb4e1a57c7920
carlos <carlos@Jedi>
2021-08-27 20:12:08 -0300

    Aula 4

--- aulas/src/main/java/aula4/Main.java
+++ aulas/src/main/java/aula4/Main.java
@@ -1,6 +1,7 @@
 package aula4;
 
 import java.awt.Color;
+import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
@@ -11,12 +12,33 @@ public class Main {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
+		JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
+		JPanel panel3 = new JPanel();
+		JPanel panel4 = new JPanel();
+		JLabel meuNome = new JLabel("Carlos Eduardo Fernandes Davila");
+		JLabel nomeMae = new JLabel("Cristiane");
+		JLabel NomePai = new JLabel("Carlos");
+		GridLayout gridLayout1 = new GridLayout(2, 1);
+		GridLayout gridLayout2 = new GridLayout(1, 2);
+		
 		frame.setSize(400,400);
-		JPanel panel = new JPanel();
-		JLabel label = new JLabel("Carlos Eduardo Fernandes Davila");
-		label.setBorder(BorderFactory.createLineBorder(Color.black));
-		frame.getContentPane().add(panel);
-		panel.add(label);
+		frame.getContentPane().setLayout(gridLayout1);
+		frame.getContentPane().add(panel1);
+		frame.getContentPane().add(panel2);
+		panel1.setBackground(Color.CYAN);
+		panel1.add(meuNome);
+		panel1.setBorder(BorderFactory.createLineBorder(Color.yellow));
+		
+		panel2.setLayout(gridLayout2);
+		panel2.setBackground(Color.yellow);
+		panel2.add(panel3);
+		panel2.add(panel4);
+		panel2.setBorder(BorderFactory.createLineBorder(Color.CYAN));
+		panel3.setBackground(Color.CYAN);
+		panel3.add(nomeMae);
+		panel4.setBackground(Color.yellow);
+		panel4.add(NomePai);
 		frame.setVisible(true);
 	}
 
