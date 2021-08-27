cdf197df9e77f2d8ed3f6a852a9b9421bdbf6202
T-Gamer <T-Gamer@DESKTOP-U26BJ62>
2021-08-27 20:18:49 -0300

    Bordas e botões de Cores diferentes

--- aulas/src/main/java/aula2/Aula4.java
+++ aulas/src/main/java/aula2/Aula4.java
@@ -1,5 +1,6 @@
 package aula2;
 import java.awt.Color;
+import java.awt.GridLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -9,12 +10,33 @@ public class Aula4 {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		JLabel label = new JLabel("Hello World");
-		label.setBorder(BorderFactory.createLineBorder(Color.blue));
-		frame.getContentPane().add(panel);
-		frame.setSize(400,300);
-		panel.add(label);
+		JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
+		JPanel panel3 = new JPanel();
+		JPanel panel4 = new JPanel();
+		JLabel Type = new JLabel("Hello World");
+		JLabel Matéria = new JLabel("POO");
+		JLabel Fase = new JLabel("Quarta Fase");
+		GridLayout gridLayout1 = new GridLayout(2, 1);
+		GridLayout gridLayout2 = new GridLayout(1, 2);
+		
+		frame.setSize(400,500);
+		frame.getContentPane().setLayout(gridLayout1);
+		frame.getContentPane().add(panel1);
+		frame.getContentPane().add(panel2);
+		panel1.setBackground(Color.RED);
+		panel1.add(Type);
+		panel1.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		panel2.setLayout(gridLayout2);
+		panel2.setBackground(Color.yellow);
+		panel2.add(panel3);
+		panel2.add(panel4);
+		panel2.setBorder(BorderFactory.createLineBorder(Color.CYAN));
+		panel3.setBackground(Color.GRAY);
+		panel3.add(Matéria);
+		panel4.setBackground(Color.DARK_GRAY);
+		panel4.add(Fase);
 		frame.setVisible(true);
 	}
-}	
+
+}
