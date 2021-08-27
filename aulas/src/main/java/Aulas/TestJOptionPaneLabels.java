2af3c0f5c327bdb1d8f9ff61940c399341b8b01c
Matheus-Alborghetti <Matheus-Alborghetti@users.noreply.github.com>
2021-08-27 19:23:06 -0300

    Box Layout

---
+++ aulas/src/main/java/Aulas/TestJOptionPaneLabels.java
@@ -0,0 +1,33 @@
+package Aulas;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class TestJOptionPaneLabels {
+
+	public static void main(String[] args) {
+		JFrame janela = new JFrame("Jnela é aqui!");
+				
+		JLabel label = new JLabel("Mensagem");
+		JLabel label2 = new JLabel("Mensagem2");
+		JLabel label3 = new JLabel("Mensagem3");
+		JLabel label4 = new JLabel("Mensagem4");
+		JLabel label5 = new JLabel("Mensagem5");
+
+		janela.getContentPane().add(label);
+		janela.getContentPane().add(label2);
+		janela.getContentPane().add(label3);
+		janela.getContentPane().add(label4);
+		janela.getContentPane().add(label5);
+		
+		janela.setLayout(new GridLayout(3, 3));
+		
+		janela.setSize(200, 200);
+		janela.setVisible(true);
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
