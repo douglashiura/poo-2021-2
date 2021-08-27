047d4736f7fc366fc1c23889ee45c385b991b820
Proctux <luiz.ado@hotmail.com>
2021-08-27 19:21:16 -0300

    feat: Add the class three exercise of grid layout

---
+++ src/main/java/com/ies/poo/memory/number/game/ClassThreeExerciciseGridLayout.java
@@ -0,0 +1,37 @@
+package com.ies.poo.memory.number.game;
+
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class ClassThreeExerciciseGridLayout {
+
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame("Age");
+		frame.setSize(600, 600);
+		
+		JLabel labelOne = new JLabel("Message One");
+		JLabel labelTwo = new JLabel("Message Two");
+		JLabel labelThree = new JLabel("Message Three");
+		JLabel labelFour = new JLabel("Message Four");
+		
+		frame.getContentPane().setLayout(new GridLayout(2, 2));
+		
+		labelOne.setSize(200, 300);
+		labelTwo.setSize(100, 200);
+		labelThree.setSize(100, 200);
+		labelFour.setSize(100, 200);
+		
+		frame.getContentPane().add(labelOne);
+		frame.getContentPane().add(labelTwo);
+		frame.getContentPane().add(labelThree);
+		frame.getContentPane().add(labelFour);
+		
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
