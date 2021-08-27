12caa29830306613fba18d9eb6b8be99b1e23a54
atn337 <38930758+atn337@users.noreply.github.com>
2021-08-27 19:41:35 -0300

    borda

--- aulas/src/test/java/aulas/GerenciadorDeLayoutNull.java
+++ aulas/src/test/java/aulas/GerenciadorDeLayoutNull.java
@@ -1,14 +1,20 @@
 package aulas;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class GerenciadorDeLayoutNull extends JButton {
 
 	public static void main(String arg[]) {
 
 		JFrame janela = new JFrame();
+		JPanel panel = new JPanel();
 
 		janela.setSize(400, 200);
 		janela.setTitle("Layout Null");
@@ -19,16 +25,21 @@ public class GerenciadorDeLayoutNull extends JButton {
 		JLabel cabeca = new JLabel("{-.-}");
 		JLabel corpo = new JLabel("l)_(l");
 		JLabel pes = new JLabel("-:- :-");
+		JLabel nome = new JLabel("Anatan");
 
 		topo.setBounds(180, -1, 50, 80);
 		cabeca.setBounds(180, 10, 50, 80);
 		corpo.setBounds(180, 21, 50, 80);
 		pes.setBounds(180, 30, 50, 80);
-		
+
 		JButton btn = new JButton("OK");
 		btn.setBounds(180, 76, 50, 80);
-		btn.setSize(23,20);
+		btn.setSize(23, 20);
 		btn.setVisible(true);
+
+		panel.setBounds(50, 60, 40, 50);
+		panel.setBorder(BorderFactory.createLineBorder(Color.black));
+		panel.add(nome);
 
 		janela.getContentPane().setLayout(null);
 		janela.getContentPane().add(topo);
@@ -36,6 +47,7 @@ public class GerenciadorDeLayoutNull extends JButton {
 		janela.getContentPane().add(corpo);
 		janela.getContentPane().add(pes);
 		janela.getContentPane().add(btn);
+		janela.getContentPane().add(panel);
 
 	}
 
