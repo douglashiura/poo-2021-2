988da3205f4044540be89b60c7c196193c477efe
Matheus-Alborghetti <Matheus-Alborghetti@users.noreply.github.com>
2021-08-27 20:21:19 -0300

    alterações no JPanel

--- aulas/src/main/java/Aulas/JPaneComBordas.java
+++ aulas/src/main/java/Aulas/JPaneComBordas.java
@@ -11,8 +11,9 @@ public class JPaneComBordas {
 		JFrame frame = new JFrame();
 		frame.setSize(300,200);
 		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createBevelBorder(3, Color.red, Color.cyan));
 		JLabel label = new JLabel("Matheus Alborghetti Machado");
-		label.setBorder(BorderFactory.createBevelBorder(5, Color.blue, Color.yellow));
+		label.setBorder(BorderFactory.createBevelBorder(3, Color.blue, Color.yellow));
 		frame.getContentPane().add(panel);
 		panel.add(label);
 		frame.setVisible(true);
---
+++ aulas/src/main/java/Aulas/JPanelNovo.java
@@ -0,0 +1,45 @@
+package Aulas;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class JPanelNovo {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
+		JPanel panel3 = new JPanel();
+		JPanel panel4 = new JPanel();
+		
+		JLabel nome = new JLabel("Matheus Alborghetti");
+		JLabel ocupacao = new JLabel("Quase DBA");
+		JLabel faculdade = new JLabel("IES");
+		GridLayout gridLayout1 = new GridLayout(2, 2);
+		GridLayout gridLayout2 = new GridLayout(2, 2);
+		
+		frame.setSize(500,500);
+		frame.getContentPane().setLayout(gridLayout1);
+		frame.getContentPane().add(panel1);
+		frame.getContentPane().add(panel2);
+		panel1.setBackground(Color.gray);
+		panel1.add(nome);
+		panel1.setBorder(BorderFactory.createLineBorder(Color.red));		
+		panel2.setLayout(gridLayout2);
+		panel2.setBackground(Color.cyan);
+		panel2.add(panel3);
+		panel2.add(panel4);
+		panel2.setBorder(BorderFactory.createLineBorder(Color.red));
+		panel3.setBackground(Color.cyan);
+		panel3.add(ocupacao);
+		panel4.setBackground(Color.yellow);
+		panel4.add(faculdade);
+		frame.setVisible(true);
+	}
+
+}
