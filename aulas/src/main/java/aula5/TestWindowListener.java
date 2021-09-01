50c80c7d3b1e6719d68a1aa4f0d37ebb4d6cc4ed
carlos <carlos@Jedi>
2021-09-01 20:27:56 -0300

    Aula 5 teste de eventos

---
+++ aulas/src/main/java/aula5/TestWindowListener.java
@@ -0,0 +1,50 @@
+package aula5;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class TestWindowListener {
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Window Listener");
+		JLabel label = new JLabel("Clique aqui!");
+		frame.setSize(200,300);
+		frame.getContentPane().add(label);
+		frame.addMouseListener(new MouseListener() {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			System.out.print("\nClicou");
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}	
+		});
+		
+		frame.setVisible(true);
+	}
+
+}
