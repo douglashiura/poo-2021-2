3d65b4a00a94dfd9a4dbb85254375c8949b82973
Matheus-Alborghetti <Matheus-Alborghetti@users.noreply.github.com>
2021-09-01 20:30:44 -0300

    Aula 4 Tela de Modal

---
+++ aulas/src/main/java/Aulas/TelaDeModal.java
@@ -0,0 +1,60 @@
+package Aulas;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.JFrame;
+
+public class TelaDeModal {
+
+	public static void main(String[] args) {
+		JFrame janela = new JFrame("HelloWorldSwig");
+		final JLabel label = new JLabel("Hello World");
+		janela.getContentPane() .add(label);
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janela.pack();
+		janela.setVisible(true);
+		janela.addWindowListener(new WindowListener() {
+			
+			@Override
+			public void windowOpened(WindowEvent e) {
+				System.out.println("A janela abriu");
+				
+			}
+			
+			@Override
+			public void windowIconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowDeiconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowDeactivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.out.println("A janela está fechando");
+				
+			}
+			
+			@Override
+			public void windowClosed(WindowEvent e) {
+				System.out.println("A janela fechou");
+				
+			}
+			
+			@Override
+			public void windowActivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			});
+		}
