776cb3d5daef07a63f14608b214fb16421a16b33
nnilocoelho <nnilocoelho@gmail.com>
2021-09-01 20:46:42 -0300

    Commit 0109: Aula 5 Events WindowListener

---
+++ aulas/src/main/java/br/com/jswing/aulas/Aula5Events.java
@@ -0,0 +1,66 @@
+package br.com.jswing.aulas;
+
+import java.awt.FlowLayout;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.JFrame;
+
+public class Aula5Events {
+
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+		frame.setTitle("Events");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 600);
+		frame.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+		frame.addWindowListener(new WindowListener() {
+			
+			@Override
+			public void windowOpened(WindowEvent arg0) {
+				System.out.println("Abrindo Janela");
+				
+			}
+			
+			@Override
+			public void windowIconified(WindowEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowDeiconified(WindowEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowDeactivated(WindowEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+				System.out.println("Fechando a Janela");
+				
+			}
+			
+			@Override
+			public void windowClosed(WindowEvent arg0) {
+				System.out.println("Janela Fechada");
+				
+			}
+			
+			@Override
+			public void windowActivated(WindowEvent arg0) {
+				System.out.println("Janela Ativa");
+				
+			}
+		});
+	}
+
+}
