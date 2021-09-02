41b1ba26f3ba5993b6ac26f2be9873989d8b8d21
Larissa <Larissa@DESKTOP-IPLM3KF>
2021-09-01 21:00:51 -0300

    tratamento de eventos

--- aulas/src/main/java/testeJOptionPane/Teste.java
+++ aulas/src/main/java/testeJOptionPane/Teste.java
@@ -1,23 +1,46 @@
 package testeJOptionPane;
 
-import java.awt.Color;
-import javax.swing.BorderFactory;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class Teste {
 	public static void main (String arg[]) {
 		JFrame frame = new JFrame("Frame");
-		JPanel panel = new JPanel();
-		JLabel label = new JLabel("Larissa Starosky");
+		JLabel label = new JLabel("TESTE", SwingConstants.CENTER);
 		
-		label.setBorder(BorderFactory.createLineBorder(Color.black));
-		frame.getContentPane().add(panel);
-		panel.add(label);
-		
+		frame.getContentPane().add(label);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 400);
 		frame.setVisible(true);
-		frame.setSize(400, 200);
+		
+		frame.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				System.out.println("mouse released");
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				System.out.println("mouse pressed");
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				System.out.println("mouse exited");
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				System.out.println("mouse entered");
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("mouse clicked");
+			}
+		});
 	}
 }
