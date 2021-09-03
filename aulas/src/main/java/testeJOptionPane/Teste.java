f708fca29d5b4009d2a530770ae395a4da12fa03
Larissa <Larissa@DESKTOP-IPLM3KF>
2021-09-03 19:52:02 -0300

    altera cor de fundo

--- aulas/src/main/java/testeJOptionPane/Teste.java
+++ aulas/src/main/java/testeJOptionPane/Teste.java
@@ -1,45 +1,31 @@
 package testeJOptionPane;
 
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.SwingConstants;
 
 public class Teste {
 	public static void main (String arg[]) {
-		JFrame frame = new JFrame("Frame");
-		JLabel label = new JLabel("TESTE", SwingConstants.CENTER);
-		
+	    JLabel label = new JLabel("TESTE", JLabel.CENTER);
+	    JFrame frame = new JFrame("Frame");
 		frame.getContentPane().add(label);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400, 400);
+		frame.setSize(400, 200);
 		frame.setVisible(true);
 		
-		frame.addMouseListener(new MouseListener() {
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				System.out.println("mouse released");
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				System.out.println("mouse pressed");
-			}
-			
+		label.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseExited(MouseEvent e) {
-				System.out.println("mouse exited");
+				label.setForeground(Color.black);
+				frame.getContentPane().setBackground(Color.white);
 			}
 			
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				System.out.println("mouse entered");
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				System.out.println("mouse clicked");
+				label.setForeground(Color.white);
+				frame.getContentPane().setBackground(Color.black);  
 			}
 		});
 	}
