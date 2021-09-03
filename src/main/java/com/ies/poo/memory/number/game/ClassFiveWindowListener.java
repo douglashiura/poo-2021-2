a62eeae05ece9e3ec74fd182cb1a558fd8443d5e
Proctux <luiz.ado@hotmail.com>
2021-09-03 19:51:19 -0300

    feat: Add MouseEventListener as homework requested

--- src/main/java/com/ies/poo/memory/number/game/ClassFiveWindowListener.java
+++ src/main/java/com/ies/poo/memory/number/game/ClassFiveWindowListener.java
@@ -4,6 +4,9 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class ClassFiveWindowListener {
@@ -11,15 +14,22 @@ public class ClassFiveWindowListener {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Janela");
 		Window window = new Window(frame);
-		HearEvent hearEvent;
-		
-		
 		
 		JButton button = new JButton("Apertou-me");
 		JLabel label = new JLabel("Luiz Fernando de Morais");
 		JTextField textField = new JTextField("Luiz Fernando de Morais");
 		label.setSize(200, 200);
 		label.setLocation(100, 100);
+		label.setOpaque(true);
+		label.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent me) {
+				label.setBackground(Color.BLACK);
+			}
+			public void mouseExited(MouseEvent me) {
+				label.setBackground(new Color(255, 255, 255));
+			}
+		});
 		button.setLocation(20, 20);
 		button.setSize(100, 100);
 		textField.setSize(100, 100);
--- src/main/java/com/ies/poo/memory/number/game/HearEvent.java
+++ src/main/java/com/ies/poo/memory/number/game/HearEvent.java
@@ -1,12 +1,24 @@
 package com.ies.poo.memory.number.game;
 
+import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.util.EventListener;
 
+import javax.swing.JLabel;
 
-public interface HearEvent extends EventListener {
 
-	public void mouseEntered(MouseEvent me);
+public class HearEvent implements EventListener {
+	private JLabel label;
 	
-	public void mouseExited(MouseEvent me);
+	public HearEvent(JLabel label) {
+		this.label = label;
+	}
+
+	public void mouseEntered(MouseEvent me) {
+		label.setBackground(Color.BLACK);
+	}
+	
+	public void mouseExited(MouseEvent me) {
+		label.setBackground(new Color(255, 255, 255));
+	}
 }
