d6077a5b90b6b69598d92a4f28c93c16790a9aa7
nnilocoelho <nnilocoelho@gmail.com>
2021-09-03 19:57:06 -0300

    Commit 03.09: Aula 6 Mouse Listener

---
+++ aulas/src/main/java/br/com/jswing/aulas/Aula6EtiquetaCor.java
@@ -0,0 +1,65 @@
+package br.com.jswing.aulas;
+
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Aula6EtiquetaCor extends JFrame implements MouseListener {
+
+	private static final long serialVersionUID = 1L;
+	private JLabel meuNome;
+
+	public Aula6EtiquetaCor() {
+
+		super("Desenhando");
+		setSize(400, 300);
+
+		meuNome = new JLabel("Muda de Cor");
+		meuNome.setSize(100, 30);
+		meuNome.setOpaque(true);
+		meuNome.setLocation(0, 0);
+		meuNome.addMouseListener(this);
+
+		getContentPane().setLayout(null);
+		getContentPane().add(meuNome);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Aula6EtiquetaCor();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		System.out.println("ok");
+		meuNome.setForeground(Color.red);
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		meuNome.setForeground(Color.black);
+
+	}
+}
