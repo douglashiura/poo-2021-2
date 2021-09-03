f8b10fbbfbd7cd2ab0e4f2d4dbce71a15abe21f6
Proctux <luiz.ado@hotmail.com>
2021-09-03 20:08:54 -0300

    feat: Add textListener example

--- src/main/java/com/ies/poo/memory/number/game/ClassFiveWindowListener.java
+++ src/main/java/com/ies/poo/memory/number/game/ClassFiveWindowListener.java
@@ -6,8 +6,15 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 import java.awt.Color;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.TextEvent;
+import java.awt.event.TextListener;
 
 public class ClassFiveWindowListener {
 
@@ -18,6 +25,7 @@ public class ClassFiveWindowListener {
 		JButton button = new JButton("Apertou-me");
 		JLabel label = new JLabel("Luiz Fernando de Morais");
 		JTextField textField = new JTextField("Luiz Fernando de Morais");
+		TextField textField2 = new TextField("Flamengo");
 		label.setSize(200, 200);
 		label.setLocation(100, 100);
 		label.setOpaque(true);
@@ -34,11 +42,42 @@ public class ClassFiveWindowListener {
 		button.setSize(100, 100);
 		textField.setSize(100, 100);
 		textField.setLocation(300, 300);
+		textField2.setSize(100, 100);
+		textField2.setLocation(100, 100);
+		textField2.addTextListener(new TextListener() {
+
+			@Override
+			public void textValueChanged(TextEvent e) {
+				System.out.println(e.toString());
+				textField2.setText("vasco");
+			}
+			
+		});
+		textField.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 		frame.setLayout(null);
 		frame.setSize(400, 400);
-		frame.getContentPane().add(button);
-		frame.getContentPane().add(label);
-		frame.getContentPane().add(textField);
+		//frame.getContentPane().add(button);
+		//frame.getContentPane().add(label);
+		//frame.getContentPane().add(textField);
+		frame.getContentPane().add(textField2);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.addWindowListener(window);
