84363d972fe39b5b125890e91bb114798eb69a68
Proctux <luiz.ado@hotmail.com>
2021-09-07 19:55:01 -0300

    feat: Add button change name when clicked

--- src/main/java/com/ies/poo/screen/GameScreen.java
+++ src/main/java/com/ies/poo/screen/GameScreen.java
@@ -8,13 +8,20 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import com.ies.poo.rules.ControlSelectedButton;
+
 public class GameScreen extends JFrame {
 	private JButton button1;
 	private JButton button2;
 	private JPanel panel;
 	
+	private ControlSelectedButton controlButtons;
+	
 	public GameScreen () {
 		super("Number memory game");
+		
+		controlButtons = new ControlSelectedButton();
+		controlButtons.setButtonName("Game");
 		
 		panel = new JPanel();
 		this.add(panel);
@@ -28,8 +35,9 @@ public class GameScreen extends JFrame {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				((JButton)	e.getSource()).setBackground(Color.BLUE);;
-				
+				((JButton)	e.getSource()).setBackground(Color.green);
+				controlButtons.alterButtonSelectedState(((JButton) e.getSource()), Boolean.TRUE);
+				((JButton)	e.getSource()).setText(controlButtons.getButtonName());
 			}
 			
 		});
@@ -39,6 +47,10 @@ public class GameScreen extends JFrame {
 		button2.setBounds(120, 10, 100, 100);
 
 		this.setBounds(250, 250, 350, 350);
+		
+		this.controlButtons.addButton(button1);
+		this.controlButtons.addButton(button2);
+		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 	}
