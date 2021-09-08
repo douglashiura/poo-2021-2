fae2296e65dc059324be7615f95bcdc20d14a529
Proctux <luiz.ado@hotmail.com>
2021-09-07 22:26:39 -0300

    feat: Add one more controller to the application

--- src/main/java/com/ies/poo/screen/GameScreen.java
+++ src/main/java/com/ies/poo/screen/GameScreen.java
@@ -15,8 +15,11 @@ public class GameScreen extends JFrame {
 	private JButton button1;
 	private JButton button2;
 	private JPanel panel;
+	private JButton buttonA;
+	private JButton buttonB;
 	
 	private ControlSelectedButton controlButtons;
+	private ControlSelectedButton controlA;
 	
 	public GameScreen () {
 		super("Number memory game");
@@ -32,6 +35,9 @@ public class GameScreen extends JFrame {
 		
 		controlButtons = new ControlSelectedButton();
 		controlButtons.setButtonName("Flop");
+		
+		controlA = new ControlSelectedButton();
+		controlA.setButtonName("River");
 		
 		panel = new JPanel();
 		this.add(panel);
@@ -50,8 +56,22 @@ public class GameScreen extends JFrame {
 
 		this.setBounds(250, 250, 350, 350);
 		
+		buttonA = new JButton("Game");
+		panel.add(buttonA);
+		buttonA.setBounds(10, 110, 100, 100);
+		
+		buttonA.addActionListener(buttonAction);
+		
+		buttonB = new JButton("Game");
+		panel.add(buttonB);
+		buttonB.setBounds(120, 110, 100, 100);
+		
+		buttonB.addActionListener(buttonAction);
+		
 		this.controlButtons.addButton(button1);
 		this.controlButtons.addButton(button2);
+		this.controlA.addButton(buttonA);
+		this.controlA.addButton(buttonB);
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
