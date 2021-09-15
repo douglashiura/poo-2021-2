4ff7679937b307bd99471b3d6f105a9370bd6c33
deiwidSITE <deiwidpetrydvd@gmail.com>
2021-09-15 20:21:33 -0300

    Exercicio Valendo Nota

--- aulas/src/test/java/testeConexaoComBancoDeDado/ConexaoComBancoDeDado.java
+++ aulas/src/test/java/testeConexaoComBancoDeDado/ConexaoComBancoDeDado.java
@@ -2,7 +2,10 @@ package testeConexaoComBancoDeDado;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConexaoComBancoDeDado {
 
@@ -24,6 +27,21 @@ public class ConexaoComBancoDeDado {
 		String instrucao = "CREATE TABLE tbl_autores (ID_Autor integer CONSTRAINT pk_id_autor PRIMARY KEY,Nome_Autor varchar(30) NOT NULL, Sobrenome_Autor varchar(40) NOT NULL,Data_Nasc date);";
 		java.sql.Statement st = conn.createStatement();
 		st.executeUpdate(instrucao);
+		conn.close();
+	}
+	
+	public static void consultaCompletaTabela() throws SQLException {
+		Connection conn = obterConexao();
+		PreparedStatement  consulta = conn.prepareStatement("Select * from tbl_autores;");
+		ResultSet resultado = consulta.executeQuery();
+		conn.close();
+	}
+	public static void consultaComFiltroTabela() throws SQLException {
+		String nome = "Deiwid";
+		Connection conn = obterConexao();
+		PreparedStatement  consulta = conn.prepareStatement("Select * from tbl_autores where Nome_Autor like '"+ nome +"';");
+		ResultSet resultado = consulta.executeQuery();
+		
 		conn.close();
 	}
 }
