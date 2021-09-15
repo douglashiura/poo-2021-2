1573284e0abf25c49a1853991ce2155aad26e7e8
T-Gamer <T-Gamer@DESKTOP-U26BJ62>
2021-09-15 20:07:52 -0300

    Conexão com o DB

--- aulas/src/main/java/aula2/exc2.java
+++ aulas/src/main/java/aula2/exc2.java
@@ -1,5 +1,65 @@
 package aula2;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class exc2 {
+public class exc2 implements KeyListener, ActionListener {
 
+	JFrame frame;
+	JTextField text;
+	JLabel label;
+	JButton button;
+
+	public exc2() {
+
+		frame = new JFrame();
+		frame.setTitle("KeyListener Test");
+		label = new JLabel();
+		text = new JTextField(15);
+		text.addKeyListener(this);
+		button = new JButton("Clear");
+		button.addActionListener(this);
+		JPanel panel = new JPanel();
+		panel.add(text);
+		panel.add(button);
+
+		frame.setLayout(new BorderLayout());
+		frame.add(label, BorderLayout.NORTH);
+		frame.add(panel, BorderLayout.SOUTH);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 100);
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent ke) {
+		label.setText("Você digitou: " + ke.getKeyChar());
+	}
+
+	@Override
+	public void keyPressed(KeyEvent ke) {
+		label.setText("Você pressionou: " + ke.getKeyChar());
+	}
+
+	@Override
+	public void keyReleased(KeyEvent ke) {
+		label.setText("" + ke.getKeyChar());
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		text.setText("");
+	}
+
+	public static void main(String args[]) {
+		new exc2();
+	}
 }
