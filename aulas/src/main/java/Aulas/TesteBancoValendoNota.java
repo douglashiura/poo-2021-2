01f3cf7764e70ccda3d4d29fe067fc347badc964
Matheus-Alborghetti <Matheus-Alborghetti@users.noreply.github.com>
2021-09-15 21:55:22 -0300

    JDBC Organizado

--- aulas/src/main/java/Aulas/TesteBancoValendoNota.java
+++ aulas/src/main/java/Aulas/TesteBancoValendoNota.java
@@ -9,15 +9,16 @@ public class TesteBancoValendoNota {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		sequelConsult();
 		sequelWithFilter();
-}
-	
-	public static Connection makeConnectiob() throws SQLException {		
+	}
+
+	public static Connection makeConnectiob() throws SQLException {
 		String url = "jdbc:postgresql://localhost:5432/testedb";
 		String user = "postgres";
-		String senha = "matheus25";		
+		String senha = "matheus25";
 		return DriverManager.getConnection(url, user, senha);
-		
+
 	}
+
 	public static void makeTable() throws SQLException {
 		Connection conn = makeConnectiob();
 		String sequel = "CREATE TABLE empresa (Id integer CONSTRAINT pk_id PRIMARY KEY,Funcionario varchar(150) NOT NULL,Setor varchar(40) NOT NULL,Funcao varchar(30);";
@@ -25,18 +26,20 @@ public class TesteBancoValendoNota {
 		st.executeUpdate(sequel);
 		conn.close();
 	}
-	
+
 	public static void sequelConsult() throws SQLException {
 		Connection conn = makeConnectiob();
-		PreparedStatement  consult = conn.prepareStatement("Select * from empresa;");
+		PreparedStatement consult = conn.prepareStatement("Select * from empresa;");
 		consult.executeQuery();
 		conn.close();
 	}
+
 	public static void sequelWithFilter() throws SQLException {
 		String Funcionario = "Matheus";
 		Connection conn = makeConnectiob();
-		PreparedStatement  consult = conn.prepareStatement("Select * from empresa where Setor like '"+ Funcionario +"';");
-		consult.executeQuery();		
+		PreparedStatement consult = conn
+				.prepareStatement("Select * from empresa where Setor like '" + Funcionario + "';");
+		consult.executeQuery();
 		conn.close();
 	}
 }
