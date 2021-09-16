52f76d6b6c5690c86129fd8dc73c95c680d961f6
T-Gamer <T-Gamer@DESKTOP-U26BJ62>
2021-09-15 21:56:56 -0300

    JDB atividade valendo aula

--- aulas/src/main/java/Conexao/Conexaao_DB.java
+++ aulas/src/main/java/Conexao/Conexaao_DB.java
@@ -1,5 +1,28 @@
 package Conexao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conexaao_DB {
 
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		criarTabela();
+	}
+	
+	public static Connection obterConexao() throws SQLException {
+		
+		String url = "jdbc:postgresql://127.0.0.1:5432/POO";
+		String usuarioDoBanco = "postgres";
+		String senha = "postgres";
+		
+		return DriverManager.getConnection(url, usuarioDoBanco, senha);
+		
+	}
+	public static void criarTabela() throws SQLException {
+		Connection conn = obterConexao();
+		String instrucao = "CREATE TABLE tbl_autores (ID_Autor integer CONSTRAINT pk_id_autor PRIMARY KEY,Nome_Autor varchar(30) NOT NULL, Sobrenome_Autor varchar(40) NOT NULL,Data_Nasc date);";
+		java.sql.Statement st = conn.createStatement();
+		st.executeUpdate(instrucao);
+		conn.close();
+	}
 }
