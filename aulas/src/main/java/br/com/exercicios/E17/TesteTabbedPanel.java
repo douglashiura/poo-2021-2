0d11ff508c776dfb543f4a5a482fd399f1255b89
Gustavo Oliveira <69910604+gugamenezes@users.noreply.github.com>
2021-09-22 21:00:39 -0300

    E17

---
+++ aulas/src/main/java/br/com/exercicios/E17/TesteTabbedPanel.java
@@ -0,0 +1,26 @@
+package br.com.exercicios.E17;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+public class TesteTabbedPanel extends JFrame{
+	
+	TabbedPane abas = new TabbedPane();
+
+	public TesteTabbedPanel() {
+		configuracao();
+		add(BorderLayout.CENTER, abas);
+	}
+
+	public void configuracao() {
+		setSize(500, 500);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		TesteTabbedPanel frame = new TesteTabbedPanel();
+	}
+
+}
