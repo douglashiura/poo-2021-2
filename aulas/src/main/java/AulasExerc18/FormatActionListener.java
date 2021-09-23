2e3ac5fe820cdebb9c7f99f9a133ad9abfec0f0c
Matheus-Alborghetti <Matheus-Alborghetti@users.noreply.github.com>
2021-09-22 21:47:45 -0300

    Exercicio18 ajustes

---
+++ aulas/src/main/java/AulasExerc18/FormatActionListener.java
@@ -0,0 +1,18 @@
+package AulasExerc18;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FormatActionListener implements ActionListener{
+	private CriarTabelaE18 bdTable;
+	
+	public FormatActionListener(CriarTabelaE18 bdTable) {
+		this.bdTable = bdTable;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		this.bdTable.conectar();
+	}
+
+}
