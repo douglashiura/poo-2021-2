728681ab2f61272e739a1c6a9798d173b5cfa9d6
douglas <douglashiura@gmail.com>
2021-09-24 20:14:36 -0300

    feat

--- aulas/src/main/java/br/ies/pooii/douglas/hiura/jdbc/ConexaoComBancoDeDados.java
+++ aulas/src/main/java/br/ies/pooii/douglas/hiura/jdbc/ConexaoComBancoDeDados.java
@@ -2,14 +2,24 @@ package br.ies.pooii.douglas.hiura.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class ConexaoComBancoDeDados {
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	public static void main(String[] args) throws Exception {
 		Class.forName("org.postgresql.Driver");
 		Connection conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/saas", "comexchain",
 				"comexchain");
+		PreparedStatement afirmacao = conexao.prepareStatement("SELECT id, nome FROM Empresa WHERE id>?");
+		afirmacao.setInt(1, 1);
+		ResultSet resultado = afirmacao.executeQuery();
+		while (resultado.next()) {
+			System.out.println(resultado.getInt(1));
+			System.out.println(resultado.getString(2));
+			System.out.println();
+		}
+
 	}
 
 }
