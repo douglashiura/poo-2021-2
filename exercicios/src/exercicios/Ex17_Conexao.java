80ffa289bcb392d08b1069e6c32da6e1eb03601f
Lucas Dias <Lucas Dias@192.168.15.9>
2021-09-28 19:18:50 -0300

    17 , 18 ,19

---
+++ exercicios/src/exercicios/Ex17_Conexao.java
@@ -0,0 +1,12 @@
+package exercicios;
+//Apenas testa o Driver jdbc
+public class Ex17_Conexao{
+	public static void main(String[] args) {
+		try{
+			Class.forName("org.postgresql.Driver");
+			System.out.println("Driver carregado com sucesso!");
+			} catch(Exception ex) {
+				System.out.println("Driver nao pode ser carregado!");
+				}
+		}
+}
---
+++ exercicios/src/exercicios/Ex18_ConexaoDB.java
@@ -0,0 +1,23 @@
+package exercicios;
+
+import java.sql.Connection;  
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Ex18_ConexaoDB  {
+	public static void main(String[] args)  {
+		String driver = "org.postgresql.Driver"; 
+		String user   = "postgres"; 
+		String senha  = "1234"; 
+		String url    = "jdbc:postgresql://localhost:5432/postgres"; 
+		try{ 
+			Class.forName(driver);  
+			Connection con= null;
+			con = (Connection) DriverManager.getConnection(url, user, senha);
+			System.out.println("Conexão realizada com sucesso."); 
+			}  catch(ClassNotFoundException ex)  {  
+				System.err.print(ex.getMessage());  
+				}   catch(SQLException e) {
+					System.err.print(e.getMessage()); 
+					}  
+		}  
+}
