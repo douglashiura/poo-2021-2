22abfc7d7695a602bc211b4802bd1c747daeb231
Proctux <luiz.ado@hotmail.com>
2021-09-29 18:48:48 -0300

    feat: Add main file to run SliderDemo exercise

---
+++ src/main/java/com/ies/poo/classes/SliderDemo.java
@@ -0,0 +1,17 @@
+// NOMES: LUIZ FERNANDO E NILO J COELHO NETO
+
+package com.ies.poo.classes;
+
+import javax.swing.JFrame;
+
+public class SliderDemo {
+
+	public static void main(String args[]) {
+		SliderFrame sliderFrame = new SliderFrame();
+		sliderFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		sliderFrame.setSize(220, 270);
+		sliderFrame.setVisible(true);
+
+	}
+
+}
