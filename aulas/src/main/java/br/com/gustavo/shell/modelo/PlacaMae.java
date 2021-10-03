7a678010e1a745137f99c9dccb05e7e3f270159b
Gustavo Oliveira <69910604+gugamenezes@users.noreply.github.com>
2021-10-03 02:41:27 -0300

    Ajustes

--- aulas/src/main/java/br/com/gustavo/shell/modelo/PlacaMae.java
+++ aulas/src/main/java/br/com/gustavo/shell/modelo/PlacaMae.java
@@ -13,20 +13,5 @@ public class PlacaMae extends InformacoesDoSistema {
 	public String getVersao() {
 		return placaMae.getSerialNumber();
 	}
-	
-	public String getModelo() {
-		return placaMae.getVersion();
-	}
-	
-	public static void main(String[] args) {
-		PlacaMae placaMae = new PlacaMae();
-		
-		
-		System.out.println(placaMae.getFabricante());
-		System.out.println(placaMae.getVersao());
-		System.out.println(placaMae.getModelo());
-
-
-	}
 
 }
