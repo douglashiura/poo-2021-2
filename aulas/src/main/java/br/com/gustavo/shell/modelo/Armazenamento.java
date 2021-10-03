437ca83b0da5d3cd13a7f64b1eee2906f372f2e4
Gustavo Oliveira <69910604+gugamenezes@users.noreply.github.com>
2021-10-03 02:37:45 -0300

    Criando classe de Armazenamento

---
+++ aulas/src/main/java/br/com/gustavo/shell/modelo/Armazenamento.java
@@ -0,0 +1,10 @@
+package br.com.gustavo.shell.modelo;
+
+public class Armazenamento extends InformacoesDoSistema {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
