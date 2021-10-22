8b7e92325255d40c785a49c13bae7830ad6c89d5
douglas <douglashiura@gmail.com>
2021-10-22 19:47:11 -0300

    refactor: remove hardcode

--- aulas/src/main/java/br/ies/pooii/douglas/hiura/jogo/par/impar/modelo/EstrategiaDaPrimeiraJogada.java
+++ aulas/src/main/java/br/ies/pooii/douglas/hiura/jogo/par/impar/modelo/EstrategiaDaPrimeiraJogada.java
@@ -1,5 +1,8 @@
 package br.ies.pooii.douglas.hiura.jogo.par.impar.modelo;
 
+import java.util.List;
+
+import br.ies.pooii.douglas.hiura.jogo.par.impar.modelo.dao.entidade.Pessoa;
 import br.ies.pooii.douglas.hiura.jogo.par.impar.modelo.dao.entidade.Rodada;
 import br.ies.pooii.douglas.hiura.jogo.par.impar.modelo.dao.hibernate.PessoaDAOHibernate;
 import br.ies.pooii.douglas.hiura.jogo.par.impar.modelo.dao.hibernate.RodadaDAOHibernate;
@@ -16,11 +19,18 @@ public class EstrategiaDaPrimeiraJogada implements EstrategiaDeAposta {
 	public void recebeUmaAposta(Aposta aposta) {
 		Rodada rodadaEntidade = rodada.comoUmaEntidade();
 		PessoaDAOHibernate daoHibernate = new PessoaDAOHibernate();
-		daoHibernate.inserir(rodadaEntidade.getJogadores().get(0));
-		daoHibernate.inserir(rodadaEntidade.getJogadores().get(1));
-		daoHibernate.inserir(rodadaEntidade.getTorcedores().get(0));
+		List<Pessoa> jogadores = rodadaEntidade.getJogadores();
+		List<Pessoa> torcedores = rodadaEntidade.getTorcedores();
+		inserirPessoas(jogadores, daoHibernate);
+		inserirPessoas(torcedores, daoHibernate);
 		RodadaDAOHibernate daoRodadaHibernate = new RodadaDAOHibernate();
 		daoRodadaHibernate.inserir(rodadaEntidade);
+	}
+
+	private void inserirPessoas(List<Pessoa> pessoas, PessoaDAOHibernate daoHibernate) {
+		for (Pessoa pessoa : pessoas) {
+			daoHibernate.inserir(pessoa);
+		}
 	}
 
 }
